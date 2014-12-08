c1866c2eea3a7f32546b29dae5336bd393ef7e94
yuichi.suzuki <jazzmaster0601@gmail.com>
2014-12-08 18:13:19 +0900

    change display name to japanese letter.

--- helloworld.py
+++ helloworld.py
@@ -7,7 +7,7 @@ class MainPage(webapp.RequestHandler):
 
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
-        self.response.out.write('Hello, webapp World! My name is yuichi suzuki!')
+        self.response.out.write(u'Hello, webapp World! My name is 鈴木 裕一!')
 
 
 application = webapp.WSGIApplication([('/', MainPage)], debug=True)
